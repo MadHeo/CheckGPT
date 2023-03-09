0b0b08d5a0c8501cd9fd78a725d66db5093aee6c
MadHeo <tnlfl288@gmail.com>
2023-03-09 21:58:52 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { Global } from "@emotion/react";
+import { GlobalStyle } from "../src/commons/globalStyles/globalStyles";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <>
+        <Global styles={GlobalStyle} />
+        <Component {...pageProps} />
+      </>
+    </RecoilRoot>
+  );
+}
---
+++ src/commons/globalStyles/globalStyles.ts
@@ -0,0 +1,39 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyle = css`
+  * {
+    margin: 0px;
+    box-sizing: border-box;
+    font-family: "Noto-500";
+  }
+
+  @font-face {
+    font-family: "Noto-700";
+    src: url("/font/NotoSansKR-Bold.otf");
+  }
+
+  @font-face {
+    font-family: "Noto-500";
+    src: url("/font/NotoSansKR-Medium.otf");
+  }
+
+  @font-face {
+    font-family: "Noto-400";
+    src: url("/font/NotoSansKR-Regular.otf");
+  }
+
+  @font-face {
+    font-family: "Roboto-700";
+    src: url("/font/Roboto-Bold.ttf");
+  }
+
+  @font-face {
+    font-family: "Roboto-500";
+    src: url("/font/Roboto-Medium.ttf");
+  }
+
+  @font-face {
+    font-family: "Roboto-400";
+    src: url("/font/Roboto-Regular.ttf");
+  }
+`;
