fff1d7fb777341c1ad8682ef68a4e0aa65febdab
MadHeo <tnlfl288@gmail.com>
2023-06-06 18:18:08 +0900

    파이그래프 레이블 추가

--- src/components/chart/pieChart/index.tsx
+++ src/components/chart/pieChart/index.tsx
@@ -2,10 +2,10 @@ import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
 const COLORS = ["#9341ff", "#ff49b6", "#d61fff", "#5537ff"];
 const data = [
-  { name: "Group A", value: 200 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
+  { name: "사용 안함", value: 200 },
+  { name: "조금", value: 300 },
+  { name: "많이", value: 300 },
+  { name: "매우 많이", value: 200 },
 ];
 
 export default function PieChartPage() {
@@ -13,14 +13,14 @@ export default function PieChartPage() {
     <PieChart width={400} height={400}>
       <Pie
         data={data}
-        cx={150}
+        cx={170}
         cy={130}
         innerRadius={50}
         outerRadius={90}
         fill="#8884d8"
         paddingAngle={0}
         dataKey="value"
-        label
+        label={({ name, value }) => `${name}: ${value}%`}
       >
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
